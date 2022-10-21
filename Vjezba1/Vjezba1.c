727ae56497d99ca030a5d830a1110463d3c31809
Martina Poljak <mpolja01@fesb.hr>
2022-10-21 13:09:10 +0200

    code refactoring and minor fixes

--- Vjezba1/Vjezba1.c
+++ Vjezba1/Vjezba1.c
@@ -3,17 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct studenti {
+typedef struct students {
 	char FirstName[50];
 	char LastName[50];
 	int Points;
 } stud;
 
+int countStudentsInFile(FILE*);
+void readStudentsFromFile(FILE*, stud*);
+
 int main()
 {
 	char buffer[150];
 	char ch;
-	int count = 0, i = 0;
+	int count;
 	FILE* fp;
 	stud* student;
 	double rel_br_bod[100];
@@ -26,30 +29,51 @@ int main()
 		return FILE_DIDNT_OPEN_ERROR;
 	}
 
+	count = countStudentsInFile(fp);
+
+	student = (stud*)malloc(sizeof(stud) * count);
+
+	readStudentsFromFile(fp, student);
+
+	for (int i = 0; i < count; i++) {
+		rel_br_bod[i] = ((double)student[i].Points / (double)max_br_bod) * 100;
+	}
+
+	printf("First name\tLast name\tPoints\t\tRelative points\n");
+
+	for (int i = 0; i < count; i++) {
+		printf("%-12s\t%-12s\t%-10d\t%-10.2lf\n", student[i].FirstName, student[i].LastName, student[i].Points, rel_br_bod[i]);
+	}
+
+	free(student);
+	fclose(fp);
+
+	return 0;
+}
+
+int countStudentsInFile(FILE *fp){
+	char buffer[150];
+	int count = 0;
+
 	while (fgets(buffer, sizeof(buffer), fp) != NULL)
 	{
-		if (strcmp(buffer, "\n") != NULL) {
+		if (strcmp(buffer, "\n") != 0) {
 			count++;
 		}
-	
-}
+
+	}
 
 	rewind(fp);
 
 	printf("The number of students in the file is > %d\n", count);
 
-	student = (stud*)malloc(sizeof(stud) * count);
+	return count;
+}
 
+void readStudentsFromFile(FILE* fp, stud* student) {
+	int i = 0;
 	while (!feof(fp)) {
 		fscanf(fp, " %s	%s	%d", student[i].FirstName, student[i].LastName, &student[i].Points);
 		i++;
 	}
-	for (i = 0; i < count; i++)
-		rel_br_bod[i] = ((double)student[i].Points / (double)max_br_bod) * 100;
-
-	for (i = 0; i < count; i++) {
-		printf("%s	%s	%d %.4lf\n", student[i].FirstName, student[i].LastName, student[i].Points, rel_br_bod[i]);
-	}
-
-	fclose(fp);
 }
